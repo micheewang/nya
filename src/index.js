37d80689838561a059663dbd12bec3b38b0c2522
micheewang <979678763@qq.com>
2021-03-15 17:32:40 +0800

    delete unuse code

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@
 import { TAG, createElement } from './element';
 import { createComponent } from './component';
 import { renderDOM } from './dom';
-import { createStore } from './store';
 import { createTruck } from './truck';
 import hooks from './hooks';
 
@@ -11,7 +10,6 @@ window.nya = Object.assign(
     createElement,
     createComponent,
     renderDOM,
-    createStore,
     createTruck,
     TAG,
   },
