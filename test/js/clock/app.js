b234fbc71999e909025cf72fe59c70cf8a4ec085
micheewang <979678763@qq.com>
2021-03-31 21:10:31 +0800

    test file

--- test/js/clock/app.js
+++
@@ -1,4 +0,0 @@
-import Clock from './clock.js';
-const { renderDOM } = nya;
-
-renderDOM(Clock(), document.getElementById('root'));
--- test/js/helloWorld.js
+++
@@ -1,4 +0,0 @@
-const { renderDOM, TAG } = nya;
-const { H1 } = TAG;
-
-renderDOM(H1('Hello, world!'), document.getElementById('root'));
